14f7e45900d01f2d0607132b880d5e84d94683a2
gox <gox.roche@gmail.com>
2020-11-05 17:59:56 +0100

    Start barcode scanner on fab click

--- app/src/main/java/com/gox/dt1helper/MainActivity.java
+++ app/src/main/java/com/gox/dt1helper/MainActivity.java
@@ -1,5 +1,6 @@
 package com.gox.dt1helper;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -18,6 +19,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.gox.dt1helper.settings.SettingsActivity;
 import com.gox.dt1helper.ui.main.SectionsPagerAdapter;
 
@@ -37,11 +40,16 @@ public class MainActivity extends AppCompatActivity {
 
     public OkHttpClient client;
     public Context context;
+    private Activity activity;
+    private int requestCode;
+    private int resultCode;
+    private Intent data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         context = this;
+        activity = this;
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -54,12 +62,11 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*Snackbar.make(view, "Call product api", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();*/
-                getProduct("3021760290040");
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setOrientationLocked(false);
+                integrator.initiateScan();
             }
         });
-
 
         ConnectionSpec spec = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
                 .tlsVersions(TlsVersion.TLS_1_2)
@@ -95,7 +102,6 @@ public class MainActivity extends AppCompatActivity {
             startActivity(myIntent);
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -131,5 +137,24 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    // Get the results of barcode scanner
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        this.requestCode = requestCode;
+        this.resultCode = resultCode;
+        this.data = data;
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Product scan cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                getProduct(result.getContents());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
